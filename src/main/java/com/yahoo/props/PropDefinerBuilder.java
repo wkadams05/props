e9ffaf4332c85d9e476bb7329821527d94b2a19c
Jaewoong Choi <jw@yahoo-inc.com>
2017-10-06 18:08:31 -0700

    Supports context change event listening framework

--- src/main/java/com/yahoo/props/PropDefinerBuilder.java
+++ src/main/java/com/yahoo/props/PropDefinerBuilder.java
@@ -1,7 +1,6 @@
 package com.yahoo.props;
 
-import static com.yahoo.props.Utils.nonNullMessage;
-import static java.util.Objects.requireNonNull;
+import com.google.common.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.HashMap;
@@ -10,158 +9,124 @@ import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
-import com.google.common.reflect.TypeToken;
+import static com.yahoo.props.Utils.nonNullMessage;
+import static java.util.Objects.requireNonNull;
 
 public class PropDefinerBuilder<CONTEXT> {
-    public static <CONTEXT> PropDefinerBuilder<CONTEXT> newBuilder(Class<CONTEXT> contextClass) {
-        return new PropDefinerBuilder<CONTEXT>();
-    }
-    
-    @FunctionalInterface
-    public interface TypeGetter<CONTEXT, TYPE> {
-        TYPE getFrom(CONTEXT context, String name);
-    }
-    
-    @FunctionalInterface
-    public interface TypeSetter<CONTEXT, TYPE> {
-        void setTo(CONTEXT context, String name, TYPE value);
-    }
-    
-    @FunctionalInterface
-    public interface ComprehensiveTypeGetter<CONTEXT, TYPE> extends TypeGetter<CONTEXT, TYPE> {
-        default TYPE getFrom(CONTEXT context, String name) {
-            throw new IllegalAccessError();
-        }
-        
-        TYPE getFrom(CONTEXT context, Type type, String name);
-    }
-    
-    @FunctionalInterface
-    public interface ComprehensiveTypeSetter<CONTEXT, TYPE> extends TypeSetter<CONTEXT, TYPE> {
-        default void setTo(CONTEXT context, String name, TYPE value) {
-            throw new IllegalAccessError();
-        }
-        
-        void setTo(CONTEXT context, Type type, String name, TYPE value);
-    }
-    
-    @FunctionalInterface
-    public interface EventHandler<CONTEXT> {
-        void onEvent(CONTEXT context, String name, Object value);
-    }
-    
-    private Map<Type, TypeGetter<CONTEXT, ?>> typeGetters = new HashMap<>();
-    private Map<Type, TypeSetter<CONTEXT, ?>> typeSetters = new HashMap<>();
-    
+    private Map<Type, TypeGetter<CONTEXT, ?>>                         typeGetters              = new HashMap<>();
+    private Map<Type, TypeSetter<CONTEXT, ?>>                         typeSetters              = new HashMap<>();
     private Map<Predicate<Type>, ComprehensiveTypeGetter<CONTEXT, ?>> comprehensiveTypeGetters = new HashMap<>();
     private Map<Predicate<Type>, ComprehensiveTypeSetter<CONTEXT, ?>> comprehensiveTypeSetters = new HashMap<>();
-    
     private EventHandler<CONTEXT> afterInitEventHandler;
     private EventHandler<CONTEXT> afterGetEventHandler;
     private EventHandler<CONTEXT> afterSetEventHandler;
-    
+
     private PropDefinerBuilder() {
     }
-    
-    public PropDefinerBuilder<CONTEXT> setObjectGetter(TypeGetter<CONTEXT, Object> objectGetter) {
-        
-        requireNonNull(objectGetter, nonNullMessage("objectGetter"));
-        
-        typeGetters.put(Object.class, objectGetter);
-        return this;
+
+    public static <CONTEXT> PropDefinerBuilder<CONTEXT> newBuilder(Class<CONTEXT> contextClass) {
+        return new PropDefinerBuilder<CONTEXT>();
     }
-    
-    public PropDefinerBuilder<CONTEXT> setObjectSetter(TypeSetter<CONTEXT, Object> objectSetter) {
-        
-        requireNonNull(objectSetter, nonNullMessage("objectSetter"));
-        
-        typeSetters.put(Object.class, objectSetter);
-        return this;
-    }
-    
+
     public <TYPE> PropDefinerBuilder<CONTEXT> setTypeGetter(Class<TYPE> typeClass,
-            TypeGetter<CONTEXT, TYPE> typeGetter) {
-        
+                                                            TypeGetter<CONTEXT, TYPE> typeGetter) {
+
         requireNonNull(typeClass, nonNullMessage("typeClass"));
         requireNonNull(typeGetter, nonNullMessage("typeGetter"));
-        
+
         typeGetters.put(typeClass, typeGetter);
         return this;
     }
-    
+
     public <TYPE> PropDefinerBuilder<CONTEXT> setTypeSetter(Class<TYPE> typeClass,
-            TypeSetter<CONTEXT, TYPE> typeSetter) {
-        
+                                                            TypeSetter<CONTEXT, TYPE> typeSetter) {
+
         requireNonNull(typeClass, nonNullMessage("typeClass"));
         requireNonNull(typeSetter, nonNullMessage("typeSetter"));
-        
+
         typeSetters.put(typeClass, typeSetter);
         return this;
     }
-    
+
     public <TYPE> PropDefinerBuilder<CONTEXT> setTypeGetter(TypeToken<TYPE> typeToken,
-            TypeGetter<CONTEXT, TYPE> typeGetter) {
-        
+                                                            TypeGetter<CONTEXT, TYPE> typeGetter) {
+
         requireNonNull(typeToken, nonNullMessage("typeToken"));
         requireNonNull(typeGetter, nonNullMessage("typeGetter"));
-        
+
         typeGetters.put(typeToken.getType(), typeGetter);
         return this;
     }
-    
+
     public <TYPE> PropDefinerBuilder<CONTEXT> setTypeSetter(TypeToken<TYPE> typeToken,
-            TypeSetter<CONTEXT, TYPE> typeSetter) {
-        
+                                                            TypeSetter<CONTEXT, TYPE> typeSetter) {
+
         requireNonNull(typeToken, nonNullMessage("typeToken"));
         requireNonNull(typeSetter, nonNullMessage("typeSetter"));
-        
+
         typeSetters.put(typeToken.getType(), typeSetter);
         return this;
     }
-    
+
     public <TYPE> PropDefinerBuilder<CONTEXT> setTypeGetter(Predicate<Type> typeFilter,
-            ComprehensiveTypeGetter<CONTEXT, TYPE> typeGetter) {
-        
+                                                            ComprehensiveTypeGetter<CONTEXT, TYPE> typeGetter) {
+
         requireNonNull(typeFilter, nonNullMessage("typeFilter"));
         requireNonNull(typeGetter, nonNullMessage("typeGetter"));
-        
+
         comprehensiveTypeGetters.put(typeFilter, typeGetter);
         return this;
     }
-    
+
     public <TYPE> PropDefinerBuilder<CONTEXT> setTypeSetter(Predicate<Type> typeFilter,
-            ComprehensiveTypeSetter<CONTEXT, TYPE> typeSetter) {
-        
+                                                            ComprehensiveTypeSetter<CONTEXT, TYPE> typeSetter) {
+
         requireNonNull(typeFilter, nonNullMessage("typeFilter"));
         requireNonNull(typeSetter, nonNullMessage("typeSetter"));
-        
+
         comprehensiveTypeSetters.put(typeFilter, typeSetter);
         return this;
     }
-    
+
     public PropDefinerBuilder<CONTEXT> setAfterInitEventHandler(EventHandler<CONTEXT> afterInitEventHandler) {
         this.afterInitEventHandler = requireNonNull(afterInitEventHandler, nonNullMessage("afterInitEventHandler"));
         return this;
     }
-    
+
     public PropDefinerBuilder<CONTEXT> setAfterGetEventHandler(EventHandler<CONTEXT> afterGetEventHandler) {
         this.afterGetEventHandler = requireNonNull(afterGetEventHandler, nonNullMessage("afterGetEventHandler"));
         return this;
     }
-    
+
     public PropDefinerBuilder<CONTEXT> setAfterSetEventHandler(EventHandler<CONTEXT> afterSetEventHandler) {
         this.afterSetEventHandler = requireNonNull(afterSetEventHandler, nonNullMessage("afterSetEventHandler"));
         return this;
     }
-    
+
     private TypeGetter<CONTEXT, Object> getObjectGetter() {
         return getTypeGetter(Object.class);
     }
-    
+
+    public PropDefinerBuilder<CONTEXT> setObjectGetter(TypeGetter<CONTEXT, Object> objectGetter) {
+
+        requireNonNull(objectGetter, nonNullMessage("objectGetter"));
+
+        typeGetters.put(Object.class, objectGetter);
+        return this;
+    }
+
     private TypeSetter<CONTEXT, Object> getObjectSetter() {
         return getTypeSetter(Object.class);
     }
-    
+
+    public PropDefinerBuilder<CONTEXT> setObjectSetter(TypeSetter<CONTEXT, Object> objectSetter) {
+
+        requireNonNull(objectSetter, nonNullMessage("objectSetter"));
+
+        typeSetters.put(Object.class, objectSetter);
+        return this;
+    }
+
     @SuppressWarnings("unchecked")
     private <TYPE> TypeGetter<CONTEXT, TYPE> getTypeGetter(Type type) {
         TypeGetter<CONTEXT, TYPE> getter = (TypeGetter<CONTEXT, TYPE>) typeGetters.get(type);
@@ -174,7 +139,7 @@ public class PropDefinerBuilder<CONTEXT> {
         }
         return getter;
     }
-    
+
     @SuppressWarnings("unchecked")
     private <TYPE> TypeSetter<CONTEXT, TYPE> getTypeSetter(Type type) {
         TypeSetter<CONTEXT, TYPE> setter = (TypeSetter<CONTEXT, TYPE>) typeSetters.get(type);
@@ -187,78 +152,127 @@ public class PropDefinerBuilder<CONTEXT> {
         }
         return setter;
     }
-    
+
     public PropDefiner<CONTEXT> build() {
         return new PropDefiner<CONTEXT>() {
-            
+
             @Override
             public <TYPE> Prop<CONTEXT, TYPE> define(String name, Class<TYPE> typeClass) {
                 return define(name, typeClass, null);
             }
-            
+
             @Override
             public <TYPE> Prop<CONTEXT, TYPE> define(String name, Class<TYPE> typeClass,
-                    Function<CONTEXT, TYPE> defaultInitializer) {
-                
+                                                     Function<CONTEXT, TYPE> defaultInitializer) {
+
                 requireNonNull(name, nonNullMessage("name"));
                 requireNonNull(typeClass, nonNullMessage("typeClass"));
-                
-                return new PropImpl<CONTEXT, TYPE>(name, typeClass, resolveTypeGetter(typeClass),
-                        resolveTypeSetter(typeClass), afterInitEventHandler, afterGetEventHandler, afterSetEventHandler,
+
+                return new PropImpl<CONTEXT, TYPE>(
+                        name,
+                        typeClass,
+                        resolveTypeGetter(typeClass),
+                        resolveTypeSetter(typeClass),
+                        resolveTypeGetter(String.class),
+                        resolveTypeSetter(String.class),
+                        afterInitEventHandler,
+                        afterGetEventHandler,
+                        afterSetEventHandler,
                         defaultInitializer);
             }
-            
+
             @Override
             public <TYPE> Prop<CONTEXT, TYPE> define(String name, TypeToken<TYPE> typeToken) {
                 return define(name, typeToken, null);
             }
-            
+
             @Override
             public <TYPE> Prop<CONTEXT, TYPE> define(String name, TypeToken<TYPE> typeToken,
-                    Function<CONTEXT, TYPE> defaultInitializer) {
-                
+                                                     Function<CONTEXT, TYPE> defaultInitializer) {
+
                 requireNonNull(name, nonNullMessage("name"));
                 requireNonNull(typeToken, nonNullMessage("typeToken"));
-                
-                return new PropImpl<CONTEXT, TYPE>(name, typeToken.getType(), resolveTypeGetter(typeToken),
-                        resolveTypeSetter(typeToken), afterInitEventHandler, afterGetEventHandler, afterSetEventHandler,
+
+                return new PropImpl<CONTEXT, TYPE>(
+                        name,
+                        typeToken.getType(),
+                        resolveTypeGetter(typeToken),
+                        resolveTypeSetter(typeToken),
+                        resolveTypeGetter(String.class),
+                        resolveTypeSetter(String.class),
+                        afterInitEventHandler,
+                        afterGetEventHandler,
+                        afterSetEventHandler,
                         defaultInitializer);
             }
-            
+
             private <TYPE> TypeGetter<CONTEXT, TYPE> resolveTypeGetter(TypeToken<TYPE> typeToken) {
                 return resolveTypeGetter(typeToken.getType());
             }
-            
+
             private <TYPE> TypeSetter<CONTEXT, TYPE> resolveTypeSetter(TypeToken<TYPE> typeToken) {
                 return resolveTypeSetter(typeToken.getType());
             }
-            
+
             @SuppressWarnings("unchecked")
             private <TYPE> TypeGetter<CONTEXT, TYPE> resolveTypeGetter(Type type) {
                 TypeGetter<CONTEXT, TYPE> typeGetter = getTypeGetter(type);
                 if (typeGetter == null) {
                     TypeGetter<CONTEXT, Object> objectGetter = getObjectGetter();
-                    
+
                     requireNonNull(objectGetter,
                             "Either objectGetter or typeGetter is required for type: " + type.getTypeName());
-                    
+
                     typeGetter = (context, name) -> (TYPE) objectGetter.getFrom(context, name);
                 }
                 return typeGetter;
             }
-            
+
             private <TYPE> TypeSetter<CONTEXT, TYPE> resolveTypeSetter(Type type) {
                 TypeSetter<CONTEXT, TYPE> typeSetter = getTypeSetter(type);
                 if (typeSetter == null) {
                     TypeSetter<CONTEXT, Object> objectSetter = getObjectSetter();
-                    
+
                     requireNonNull(objectSetter,
                             "Either objectSetter or typeSetter is required for type: " + type.getTypeName());
-                    
+
                     typeSetter = (context, name, value) -> objectSetter.setTo(context, name, value);
                 }
                 return typeSetter;
             }
         };
+    }
+
+    @FunctionalInterface
+    public interface TypeGetter<CONTEXT, TYPE> {
+        TYPE getFrom(CONTEXT context, String name);
+    }
+
+    @FunctionalInterface
+    public interface TypeSetter<CONTEXT, TYPE> {
+        void setTo(CONTEXT context, String name, TYPE value);
+    }
+
+    @FunctionalInterface
+    public interface ComprehensiveTypeGetter<CONTEXT, TYPE> extends TypeGetter<CONTEXT, TYPE> {
+        default TYPE getFrom(CONTEXT context, String name) {
+            throw new IllegalAccessError();
+        }
+
+        TYPE getFrom(CONTEXT context, Type type, String name);
+    }
+
+    @FunctionalInterface
+    public interface ComprehensiveTypeSetter<CONTEXT, TYPE> extends TypeSetter<CONTEXT, TYPE> {
+        default void setTo(CONTEXT context, String name, TYPE value) {
+            throw new IllegalAccessError();
+        }
+
+        void setTo(CONTEXT context, Type type, String name, TYPE value);
+    }
+
+    @FunctionalInterface
+    public interface EventHandler<CONTEXT> {
+        void onEvent(CONTEXT context, String name, Object value);
     }
 }
--- src/test/java/com/yahoo/props/samples/config_from_properties/ConfigTest.java
+++ src/test/java/com/yahoo/props/samples/config_from_properties/ConfigTest.java
@@ -1,23 +1,16 @@
 package com.yahoo.props.samples.config_from_properties;
 
-import static com.yahoo.props.samples.config_from_properties.Config.AVAILABILITY;
-import static com.yahoo.props.samples.config_from_properties.Config.CNAME;
-import static com.yahoo.props.samples.config_from_properties.Config.ENV;
-import static com.yahoo.props.samples.config_from_properties.Config.MAIN;
-import static com.yahoo.props.samples.config_from_properties.Config.PORT;
-import static com.yahoo.props.samples.config_from_properties.Config.REPLICAS;
-import static com.yahoo.props.samples.config_from_properties.Config.ROLES;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
+import com.google.common.collect.Sets;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.Properties;
 import java.util.Set;
 
-import org.testng.annotations.Test;
-
-import com.google.common.collect.Sets;
+import static com.yahoo.props.samples.config_from_properties.Config.*;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class ConfigTest {
     @Test
@@ -28,50 +21,65 @@ public class ConfigTest {
                 + "replicas=BF1,NE1\n"//
                 + "env=stage" //
         ));
-        
+
         assertEquals(CNAME.getFrom(configs), "v1.yahooapis.com");
         assertEquals(MAIN.getFrom(configs), Colo.GQ1);
         assertEquals(REPLICAS.getFrom(configs).size(), 2);
         assertTrue(REPLICAS.getFrom(configs).contains(Colo.BF1));
         assertTrue(REPLICAS.getFrom(configs).contains(Colo.NE1));
-        assertEquals(AVAILABILITY.getFrom(configs), 0.0d);// default
+        assertEquals(AVAILABILITY.getFrom(configs), 0.2d);// 2 out of 10 (initial)
         assertTrue(PORT.isAbsent(configs));
         assertTrue(!PORT.isPresent(configs));
         assertTrue(ROLES.isAbsent(configs));
         assertTrue(!ROLES.isPresent(configs));
         assertTrue(ENV.isPresent(configs));
         assertEquals(ENV.getFrom(configs), Env.STAGE);
-        
+
         CNAME.setTo(configs, "v2.yahooapis.com");
         assertEquals(configs.getProperty("cname"), "v2.yahooapis.com");
         assertEquals(CNAME.getFrom(configs), "v2.yahooapis.com");
-        
+
         MAIN.setTo(configs, Colo.BF1);
         assertEquals(configs.getProperty("main"), "BF1");
         assertEquals(MAIN.getFrom(configs), Colo.BF1);
-        
+
         REPLICAS.setTo(configs, Sets.newHashSet(Colo.GQ1, Colo.NE1, Colo.CH1, Colo.IR2));
         assertEquals(configs.getProperty("replicas"), "CH1,GQ1,IR2,NE1");
         Set<Colo> replicas = REPLICAS.getFrom(configs);
         assertEquals(replicas.size(), 4);
-        
+
         PORT.setToIfPresent(configs, 9999);
         // unchanged since it's absent
         assertTrue(PORT.isAbsent(configs));
         PORT.setTo(configs, 4080);
         assertEquals(configs.getProperty("port"), "4080");
         assertTrue(PORT.getFrom(configs) == 4080);
-        
-        AVAILABILITY.setToIfAbsent(configs, 0.999d);
-        // unchanged since initialized with default
-        assertEquals(configs.getProperty("availability"), "0.0");
-        AVAILABILITY.setTo(configs, 0.12345678d);
-        assertEquals(configs.getProperty("availability"), "0.12345678");
-        
-        AVAILABILITY.setTo(configs, 123e-2);
-        assertEquals(configs.getProperty("availability"), "1.23");
-        
+
         ENV.setTo(configs, Env.PROD);
         assertEquals(ENV.getFrom(configs), Env.PROD);
+
+        // no-op since availability has value
+        AVAILABILITY.setToIfAbsent(configs, 0.999d);
+
+        // target monitoring test
+        assertEquals(AVAILABILITY.getFrom(configs), 0.4d);
+        REPLICAS.setTo(configs, Sets.newHashSet(Colo.GQ1, Colo.NE1, Colo.CH1));
+        assertEquals(AVAILABILITY.getFrom(configs), 0.3d);
+        REPLICAS.setTo(configs, Sets.newHashSet(Colo.GQ1, Colo.NE1, Colo.CH1, Colo.SG3, Colo.TW1));
+        assertEquals(AVAILABILITY.getFrom(configs), 0.5d);
+
+        AVAILABILITY.setTo(configs, 0.12345678d);
+        assertEquals(configs.getProperty("availability"), "0.12345678");
+        assertEquals(AVAILABILITY.getFrom(configs), 0.12345678d);
+
+        AVAILABILITY.setTo(configs, 123e-2d);
+        assertEquals(configs.getProperty("availability"), "1.23");
+        assertEquals(AVAILABILITY.getFrom(configs), 123e-2d);
+
+        REPLICAS.setTo(configs, null);
+        assertEquals(AVAILABILITY.getFrom(configs), 0.0d);
+        REPLICAS.setTo(configs, Sets.newHashSet(Colo.GQ1, Colo.NE1, Colo.CH1, Colo.SG3, Colo.TW1));
+        assertEquals(AVAILABILITY.getFrom(configs), 0.5d);
+
     }
 }
